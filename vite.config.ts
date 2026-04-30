31645f65ec2a60ad6fed54f8dbcae8d9914177fe
小墨 <codingzhang@126.com>
2026-04-30 18:06:04 +0800

    chore: configure base path for GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // docs:
+  // - GITHUB_ACTIONS: https://docs.github.com/zh/actions/reference/workflows-and-actions/variables
+  // - GITHUB_REPOSITORY: https://docs.github.com/zh/codespaces/developing-in-a-codespace/default-environment-variables-for-your-codespace
+  base: process.env.GITHUB_ACTIONS
+    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/`
+    : '/',
   plugins: [
     vue(),
     vueDevTools(),

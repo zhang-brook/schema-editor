420c3978cc71c35bf750201697a6e20e02d56a98
Brook Zhang <codingzhang@126.com>
2026-09-12 00:22:18 +0800

    feat: 更新日志链接定位到当前版本对应小节

--- src/components/modal/AboutModal.vue
+++ src/components/modal/AboutModal.vue
@@ -3,7 +3,7 @@ import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { version } from '@/../package.json'
 import { BUILD_TIME, COMMIT_ID } from '@/utils/build-info'
-import { GITHUB_REPO_URL, CHANGELOG_URL } from '@/utils/constants'
+import { GITHUB_REPO_URL, changelogUrl } from '@/utils/constants'
 import { useEscClose } from '@/composables/useEscClose'
 import { useEnterConfirm } from '@/composables/useEnterConfirm'
 
@@ -90,7 +90,7 @@ const commitUrl = computed(() =>
           {{ $t('about.github') }}
         </a>
         <a
-          :href="CHANGELOG_URL"
+          :href="changelogUrl(version)"
           target="_blank"
           rel="noopener noreferrer"
           class="about-github-link"
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
 /** GitHub 仓库地址 */
 export const GITHUB_REPO_URL = 'https://github.com/zhang-brook/schema-editor'
 
-/** 更新日志地址 */
-export const CHANGELOG_URL = `${GITHUB_REPO_URL}/blob/main/CHANGELOG.md`
+/** 更新日志地址，带版本锚点定位到对应小节（GitHub 锚点会去掉点号，如 v0.3.3 → #v033） */
+export function changelogUrl(version: string): string {
+  return `${GITHUB_REPO_URL}/blob/main/CHANGELOG.md#v${version.replace(/\./g, '').toLowerCase()}`
+}
